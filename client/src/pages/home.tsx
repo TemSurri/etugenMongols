d935b451cf317e57d0e0cd6d969ccb4b795ddfec
Temsurri <tarantem@icloud.com>
2026-05-12 17:19:16 -0400

    commented out dangling imports

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -4,25 +4,25 @@ import { useState } from "react";
 
 import Header from "../components/home/Header";
 import Hero from "../components/home/Hero";
-import Gallery from "../components/home/GalleryBar";
-import CTA from "../components/home/Contact";
-import Upcoming from "../components/home/upcoming/Upcoming";
+//import Gallery from "../components/home/GalleryBar";
+//import CTA from "../components/home/Contact";
+//import Upcoming from "../components/home/upcoming/Upcoming";
 import Footer from "../components/home/Footer";
 
 // upcoming event data
-import { events as upcomingEventsRaw, getListings } from "../static_events";
+//import { events as upcomingEventsRaw, getListings } from "../static_events";
 
 // gallery event data
-import {
-  events as galleryEventsRaw,
-  getCardInfos as getGalleryCardInfos,
-} from "../static_gallery";
+//import {
+//  events as galleryEventsRaw,
+//  getCardInfos as getGalleryCardInfos,
+//} from "../static_gallery";
 
 type Lang = "en" | "mn";
 
 export default function Home() {
-  const volunteerListings = getListings(upcomingEventsRaw);
-  const galleryItems = getGalleryCardInfos(galleryEventsRaw);
+  //const volunteerListings = getListings(upcomingEventsRaw);
+  //const galleryItems = getGalleryCardInfos(galleryEventsRaw);
 
   const [lang, setLang] = useState<Lang>("mn");
 
